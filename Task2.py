ff60024c1c68284bc45177d77cb658a4944d039f
eyecand <eerokhov@inbox.ru>
2019-03-05 00:14:58 +0300

    Чтобы листать нажимай любую клавишу, выйти из программы q + увеличил пробелы между каталогами Ps надеюсь ты читаешь комментарий

--- Task2.py
+++ Task2.py
@@ -1,20 +1,27 @@
 import os
+import re, sys, msvcrt
 
 cls = lambda: os.system('cls')
 cls()
 
 def walk(dir):
+
   for name in os.listdir(dir):
     path = os.path.join(dir, name)
     number_file = len(os.listdir(dir))
+    Key = msvcrt.getch()
+    if Key == b'q':
+        sys.exit()
     if os.path.isfile(path):
-        number = len(os.listdir(os.path.split(dir)[0]))
-        print(path + "/"  + str(number_file) + "/" + str(number)+ '/'  + str(abs(int(number_file)- int(number)))+"\n")
+            #array.append(path)
+            number = len(os.listdir(os.path.split(dir)[0]))
+            print(re.sub(r"(\\)", '  \\1', path) + "\ " + str(number_file) + " \ " + str(number)+ ' \ '  + str(abs(int(number_file)- int(number)))+"\n")
+
     else:
-        walk(path)
-
-path =r"C:\Users\ЕГОР\PycharmProjects\new1"
-
+            walk(path)
+print("Enter the path: ")
+path = input()
+print("Press to continue any Key or exit is 'q' ")
 
 walk(path)
 
